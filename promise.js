c7a26db08e0737fc810cc226270de16ab5e2c915
费习函 <physihan@outlook.com>
2017-08-17 12:00:09 +0800

    添加注释

--- promise.js
+++ promise.js
@@ -1,3 +1,9 @@
+// 这种方式只适合于不用promise直接处理数据返回值的方式，比如通过yield方式处理相邻的数据
+// x = yield promise_maker(100)
+// y = x + yield promise_maker(200)
+// z = yield promise_maker(300)
+// return [x,y,z]
+// 这种generator就不适合
 let p1 = new Promise(function (resolve, reject) {
   setTimeout(function () {
     resolve(1)
@@ -9,11 +15,10 @@ let p2 = new Promise(function (resolve, reject) {
   }, 0)
 })
 let p3 = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve(3)
-    }, 0)
-  })
-Promise.all([p1,p2,p3]).then(function(data){
-    console.log(data)
+  setTimeout(function () {
+    resolve(3)
+  }, 0)
 })
-
+Promise.all([p1, p2, p3]).then(function (data) {
+  console.log(data)
+})
