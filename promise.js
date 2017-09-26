c8016fdbe72495ddba5499c67dcb83cf7d54f62e
费习函 <physihan@outlook.com>
2017-09-26 10:09:11 +0800

    添加promise

--- promise.js
+++ promise.js
@@ -22,3 +22,99 @@ let p3 = new Promise(function (resolve, reject) {
 Promise.all([p1, p2, p3]).then(function (data) {
   console.log(data)
 })
+// 实现promise
+var PENDING = 0;
+var FULLFILLed = 1;
+var REJECTED = 2;
+
+function MyPromise(fn) {
+  var state = PENDING;
+  var value = null;
+  // 存储成功或者失败的处理器由then或done带来的
+  var handlers = [];
+
+  function fullfill(result) {
+    state = FULLFILLed;
+    value = result;
+  }
+
+  function reject(error) {
+    state = REJECTED;
+    value = error;
+  }
+
+  function resolve(result) {
+    try {
+      var then = getThen(result);
+      if (then) {
+        doResolve(then.bind(result), resolve, reject);
+        return;
+      }
+      fullfill(result);
+      console.log(value,state);
+    } catch (e) {
+      reject(e);
+    }
+  }
+
+  function handle(handler) {
+    if (state === PENDING) {
+      handlers.push(handler)
+    } else {
+      if (state === FULLFILLed && typeof handler.onFullfilled === 'function') {
+        handler.onFullfilled(value)
+      }
+      if (state === REJECTED && typeof handler.onRejected === 'function') {
+        handler.onRejected(value)
+      }
+    }
+  }
+  this.done = function (onFullfilled, onRejected) {
+    setTimeout(function () {
+      handle({
+        onFullfilled,
+        onRejected
+      })
+    }, 0)
+
+  }
+  doResolve(fn, resolve, reject)
+  this.state={state,value}
+
+}
+
+function getThen(value) {
+  var t = typeof value;
+  if (value && (t === 'object' || t === 'function')) {
+    var then = value.then;
+    if (typeof then === 'function') {
+      return then;
+    }
+  }
+}
+
+function doResolve(fn, onFullfilled, onRejected) {
+  var done = false;
+  try {
+    fn(
+      function (value) {
+        if (done) return;
+        done = true;
+        onFullfilled(value)
+      },
+      function (reason) {
+        if (done) return
+        done = true
+        onRejected(reason)
+      }
+    );
+  } catch (e) {
+    if (done) return
+    done = true
+    onRejected(e)
+  }
+}
+var p=new MyPromise(function(resolve,reject){
+  resolve(1)
+})
+console.log(p.state);
