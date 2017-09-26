6a6cfe9832b48b512be6d6b07a52351e20e75842
费习函 <physihan@outlook.com>
2017-09-26 11:29:31 +0800

    fix

--- promise.js
+++ promise.js
@@ -4,27 +4,27 @@
 // z = yield promise_maker(300)
 // return [x,y,z]
 // 这种generator就不适合
-let p1 = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve(1)
-  }, 10000)
-})
-let p2 = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve(2)
-  }, 0)
-})
-let p3 = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve(3)
-  }, 0)
-})
-Promise.all([p1, p2, p3]).then(function (data) {
-  console.log(data)
-})
+// let p1 = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve(1)
+//   }, 10000)
+// })
+// let p2 = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve(2)
+//   }, 0)
+// })
+// let p3 = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve(3)
+//   }, 0)
+// })
+// Promise.all([p1, p2, p3]).then(function (data) {
+//   console.log(data)
+// })
 // 实现promise
 var PENDING = 0;
-var FULLFILLed = 1;
+var FULFILLed = 1;
 var REJECTED = 2;
 
 function MyPromise(fn) {
@@ -33,8 +33,8 @@ function MyPromise(fn) {
   // 存储成功或者失败的处理器由then或done带来的
   var handlers = [];
 
-  function fullfill(result) {
-    state = FULLFILLed;
+  function fulfill(result) {
+    state = FULFILLed;
     value = result;
   }
 
@@ -50,8 +50,8 @@ function MyPromise(fn) {
         doResolve(then.bind(result), resolve, reject);
         return;
       }
-      fullfill(result);
-      console.log(value,state);
+      fulfill(result);
+      console.log(value, state);
     } catch (e) {
       reject(e);
     }
@@ -59,56 +59,55 @@ function MyPromise(fn) {
 
   function handle(handler) {
     if (state === PENDING) {
-      handlers.push(handler)
+      handlers.push(handler);
     } else {
-      if (state === FULLFILLed && typeof handler.onFullfilled === 'function') {
-        handler.onFullfilled(value)
+      if (state === FULFILLed && typeof handler.onFulfilled === 'function') {
+        handler.onFulfilled(value);
       }
       if (state === REJECTED && typeof handler.onRejected === 'function') {
-        handler.onRejected(value)
+        handler.onRejected(value);
       }
     }
   }
-  this.done = function (onFullfilled, onRejected) {
-    setTimeout(function () {
+  this.done = function(onFulfilled, onRejected) {
+    setTimeout(function() {
       handle({
-        onFullfilled,
+        onFulfilled,
         onRejected
-      })
-    }, 0)
-
-  }
-  this.then=function(onFullfilled,onRejected){
-    var self=this
-    return new MyPromise(function(resolve,reject){
-      return self.done(function(result){
-        if(typeof onFullfilled==='function'){
-          try {
-            return resolve(onFullfilled(result))
-          } catch (error) {
-            return resolve(error)
-            
+      });
+    }, 0);
+  };
+  this.then = function(onFulfilled, onRejected) {
+    var self = this;
+    return new MyPromise(function(resolve, reject) {
+      return self.done(
+        function(result) {
+          if (typeof onFulfilled === 'function') {
+            try {
+              return resolve(onFulfilled(result));
+            } catch (error) {
+              return resolve(error);
+            }
+          } else {
+            return resolve(result);
           }
-        }else{
-          return resolve(result)
-        }
-      },function(e){
-        if(typeof onRejected==='function'){
-          try{
-            return resolve(onRejected(e))
-          }catch(ex){
-            return reject(ex)
+        },
+        function(e) {
+          if (typeof onRejected === 'function') {
+            try {
+              return resolve(onRejected(e));
+            } catch (ex) {
+              return reject(ex);
+            }
+          } else {
+            return reject(error);
           }
-        }else{
-          return reject(error)
         }
-
-      })
-    })
-  }
-  doResolve(fn, resolve, reject)
-  this.state={state,value}
-
+      );
+    });
+  };
+  doResolve(fn, resolve, reject);
+  this.state = { state, value };
 }
 
 function getThen(value) {
@@ -121,28 +120,29 @@ function getThen(value) {
   }
 }
 
-function doResolve(fn, onFullfilled, onRejected) {
+function doResolve(fn, onFulfilled, onRejected) {
   var done = false;
   try {
     fn(
-      function (value) {
+      function(value) {
         if (done) return;
         done = true;
-        onFullfilled(value)
+        onFulfilled(value);
       },
-      function (reason) {
-        if (done) return
-        done = true
-        onRejected(reason)
+      function(reason) {
+        if (done) return;
+        done = true;
+        onRejected(reason);
       }
     );
   } catch (e) {
-    if (done) return
-    done = true
-    onRejected(e)
+    if (done) return;
+    done = true;
+    onRejected(e);
   }
 }
-var p=new MyPromise(function(resolve,reject){
-  resolve(1)
-})
-console.log(p.state);
+var p = new MyPromise(function(resolve, reject) {
+  resolve(1);
+});
+
+console.log(p.then(1, 2).then(1));
